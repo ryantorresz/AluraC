797340b16675910ecc9bd9e0f13045883b8e00f6
Ryan Torres <ryantorres77690@gmail.com>
2025-08-14 13:45:39 -0300

    primeiro código C

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("*****************************\n");
+    printf("Bem vindo ao nosso jogo de adivinhação\n");
+    printf("*****************************\n");
+
+    int numeroSecreto = 42;
+
+    printf("Qual é o número secreto?\n");
+}
