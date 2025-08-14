9a113c7a46ae49e53187661a7fc51c18da1e2320
Ryan Torres <ryantorres77690@gmail.com>
2025-08-14 14:47:25 -0300

    adicionando for

--- main.c
+++ main.c
@@ -4,16 +4,26 @@ int main() {
     int numeroSecreto = 42;  // Número a ser adivinhado
     int chute;
 
-    printf("Adivinhe o número secreto (1-100): ");
-    scanf("%d", &chute);
+    for(int i = 0; i <= 3; i++) {
+        printf("Tentativa %d de 3\n", i);
+        printf("Adivinhe o número secreto (1-100): ");
+        scanf("%d", &chute);
 
-    printf("Você chutou: %d\n", chute);
+        printf("Você chutou: %d\n", chute);
 
-    if (chute == numeroSecreto) {
-        printf("Parabéns! Acertou!\n");
-    } else {
-        printf("Errou! O número era %d\n", numeroSecreto);
+        if (chute == numeroSecreto) {
+            printf("Parabéns! Acertou!\n");
+            return 0;  // Termina o programa se acertar
+        } else {
+            if (chute > numeroSecreto) {
+                printf("Muito alto! Tente novamente.\n");
+            } else {  // Não precisa do if, pois já sabemos que é menor
+                printf("Muito baixo! Tente novamente.\n");
+            }
+        }   
     }
-
+    
+    printf("Fim do jogo.\n");
     return 0;
 }
+// Fim do código
