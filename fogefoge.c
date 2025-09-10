acdc64d37a4f1a1fe465c6763bf7e944b0b5b1c8
Ryan Torres <ryantorres77690@gmail.com>
2025-09-10 20:56:06 -0300

    melhorando foge-foge

--- fogefoge.c
+++ fogefoge.c
@@ -1,155 +1,149 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "time.h"
+#include <time.h>
 #include "fogefoge.h"
 #include "mapa.h"
 #include "ui.h"
+
+// Definições de comandos
+#define ESQUERDA 'a'
+#define CIMA     'w'
+#define BAIXO    's'
+#define DIREITA  'd'
+#define BOMBA    'b'
 
 MAPA m;
 POSICAO heroi;
 int tempilula = 0;
 
+// Verifica se o jogo acabou
 int acabou() {
-	POSICAO pos;
-
-	int perdeu = !encontramapa(&m, &pos, HEROI);
-	int ganhou = !encontramapa(&m, &pos, FANTASMA);
-
-	return ganhou || perdeu;
-		
+    POSICAO pos;
+    int perdeu = !encontramapa(&m, &pos, HEROI);
+    int ganhou = !encontramapa(&m, &pos, FANTASMA);
+    return ganhou || perdeu;
 }
 
+// Verifica se o comando é uma direção válida
 int ehdirecao(char direcao) {
-	return
-		direcao == ESQUERDA || 
-		direcao == CIMA ||
-		direcao == BAIXO ||
-		direcao == DIREITA;
+    return direcao == ESQUERDA || direcao == CIMA || direcao == BAIXO || direcao == DIREITA;
 }
 
+// Verifica se é parede
+int ehparede(MAPA* m, int x, int y) {
+    return m->matriz[x][y] == PAREDE_VERTICAL || m->matriz[x][y] == PAREDE_HORIZONTAL;
+}
+
+// Move o herói
 void move(char direcao) {
+    int proximox = heroi.x;
+    int proximoy = heroi.y;
 
-	int proximox = heroi.x;
-	int proximoy = heroi.y;
+    switch(direcao) {
+        case ESQUERDA: proximoy--; break;
+        case CIMA:     proximox--; break;
+        case BAIXO:    proximox++; break;
+        case DIREITA:  proximoy++; break;
+    }
 
-	switch(direcao) {
-		case ESQUERDA:
-			proximoy--;
-			break;
-		case CIMA:
-			proximox--;
-			break;
-		case BAIXO:
-			proximox++;
-			break;
-		case DIREITA:
-			proximoy++;
-			break;
-	}
+    if(!podeandar(&m, HEROI, proximox, proximoy))
+        return;
 
-	if(!podeandar(&m, HEROI, proximox, proximoy))
-		return;
+    if(ehpersonagem(&m, PILULA, proximox, proximoy)) {
+        tempilula = 1;
+    }
 
-	if(ehpersonagem(&m, PILULA, proximox, proximoy)) {
-		tempilula=1;
-	}
-
-	andanomapa(&m, heroi.x, heroi.y, proximox, proximoy);
-	heroi.x = proximox;
-	heroi.y = proximoy;
+    andanomapa(&m, heroi.x, heroi.y, proximox, proximoy);
+    heroi.x = proximox;
+    heroi.y = proximoy;
 }
 
-int praondefantasmavai(int xatual, int yatual, 
-	int* xdestino, int* ydestino) {
+// Decide para onde o fantasma vai
+int praondefantasmavai(int xatual, int yatual, int* xdestino, int* ydestino) {
+    int opcoes[4][2] = {
+        { xatual   , yatual+1 },
+        { xatual+1 , yatual   },
+        { xatual   , yatual-1 },
+        { xatual-1 , yatual   }
+    };
 
-	int opcoes[4][2] = { 
-		{ xatual   , yatual+1 }, 
-		{ xatual+1 , yatual   },  
-		{ xatual   , yatual-1 }, 
-		{ xatual-1 , yatual   }
-	};
-
-	srand(time(0));
-	for(int i = 0; i < 10; i++) {
-		int posicao = rand() % 4;
-
-		if(podeandar(&m, FANTASMA, opcoes[posicao][0], opcoes[posicao][1])) {
-			*xdestino = opcoes[posicao][0];
-			*ydestino = opcoes[posicao][1];
-			return 1;
-		}
-	}
-
-	return 0;
+    for(int i = 0; i < 10; i++) {
+        int posicao = rand() % 4;
+        if(podeandar(&m, FANTASMA, opcoes[posicao][0], opcoes[posicao][1])) {
+            *xdestino = opcoes[posicao][0];
+            *ydestino = opcoes[posicao][1];
+            return 1;
+        }
+    }
+    return 0;
 }
 
+// Move todos os fantasmas
 void fantasmas() {
-	MAPA copia;
+    MAPA copia;
+    copiamapa(&copia, &m);
 
-	copiamapa(&copia, &m);
-
-	for(int i = 0; i < copia.linhas; i++) {
-		for(int j = 0; j < copia.colunas; j++) {
-			if(copia.matriz[i][j] == FANTASMA) {
-
-				int xdestino;
-				int ydestino;
-
-				int encontrou = praondefantasmavai(i, j, &xdestino, &ydestino);
-
-				if(encontrou) {
-					andanomapa(&m, i, j, xdestino, ydestino);
-				}
-			}
-		}
-	}
-
-	liberamapa(&copia);
+    for(int i = 0; i < copia.linhas; i++) {
+        for(int j = 0; j < copia.colunas; j++) {
+            if(copia.matriz[i][j] == FANTASMA) {
+                int xdestino, ydestino;
+                int encontrou = praondefantasmavai(i, j, &xdestino, &ydestino);
+                if(encontrou) {
+                    andanomapa(&m, i, j, xdestino, ydestino);
+                }
+            }
+        }
+    }
+    liberamapa(&copia);
 }
 
+// Função recursiva para explosão da pílula
 void explodepilula2(int x, int y, int somax, int somay, int qtd) {
+    if(qtd == 0) return;
 
-	if(qtd == 0) return;
+    int novox = x + somax;
+    int novoy = y + somay;
 
-	int novox = x+somax;
-	int novoy = y+somay;
+    if(!ehvalida(&m, novox, novoy)) return;
+    if(ehparede(&m, novox, novoy)) return;
 
-	if(!ehvalida(&m, novox, novoy)) return;
-	if(ehparede(&m, novox, novoy)) return;
-
-	m.matriz[novox][novoy] = VAZIO;
-	explodepilula2(novox, novoy, somax, somay, qtd-1);
+    m.matriz[novox][novoy] = VAZIO;
+    explodepilula2(novox, novoy, somax, somay, qtd-1);
 }
 
+// Explode a pílula nas quatro direções
 void explodepilula() {
-	if(!tempilula) return;
-	
-	explodepilula2(heroi.x, heroi.y, 0, 1, 3);
-	explodepilula2(heroi.x, heroi.y, 0, -1, 3);
-	explodepilula2(heroi.x, heroi.y, 1, 0, 3);
-	explodepilula2(heroi.x, heroi.y, -1, 0, 3);
-	
-	tempilula = 0;
+    if(!tempilula) return;
+
+    explodepilula2(heroi.x, heroi.y, 0, 1, 3);
+    explodepilula2(heroi.x, heroi.y, 0, -1, 3);
+    explodepilula2(heroi.x, heroi.y, 1, 0, 3);
+    explodepilula2(heroi.x, heroi.y, -1, 0, 3);
+
+    tempilula = 0;
 }
 
 int main() {
-	
-	lemapa(&m);
-	encontramapa(&m, &heroi, HEROI);
+    srand(time(0)); // Inicializa o gerador de números aleatórios
 
-	do {
-		printf("Pílula: %s\n", (tempilula ? "SIM" : "NÃO"));
-		imprimemapa(&m);
+    lemapa(&m);
+    encontramapa(&m, &heroi, HEROI);
 
-		char comando;
-		scanf(" %c", &comando);
+    do {
+        printf("Pílula: %s\n", (tempilula ? "SIM" : "NÃO"));
+        imprimemapa(&m);
 
-		if(ehdirecao(comando)) move(comando);
-		if(comando == BOMBA) explodepilula();
+        char comando;
+        scanf(" %c", &comando);
 
-		fantasmas();
+        if(ehdirecao(comando)) move(comando);
+        if(comando == BOMBA) explodepilula();
 
-	} while (!acabou());
+        fantasmas();
 
-	liberamapa(&m);
+    } while (!acabou());
+
+    liberamapa(&m);
+    printf("Fim de jogo!\n");
+    return 0;
 }
--- mapa.c
+++ mapa.c
@@ -1,101 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "mapa.h"
 #include <string.h>
+#include "mapa.h"
 
+// Lê o mapa do arquivo texto
 void lemapa(MAPA* m) {
-	FILE* f;
-	f = fopen("mapa.txt", "r");
-	if(f == 0) {
-		printf("Erro na leitura do mapa");
-		exit(1);
-	}
+    FILE* f = fopen("mapa.text", "r");
+    if(f == NULL) {
+        printf("Erro na leitura do mapa (arquivo 'mapa.text' não encontrado)\n");
+        exit(1);
+    }
 
-	fscanf(f, "%d %d", &(m->linhas), &(m->colunas));
-	alocamapa(m);
-	
-	for(int i = 0; i < m->linhas; i++) {
-		fscanf(f, "%s", m->matriz[i]);
-	}
+    fscanf(f, "%d %d", &(m->linhas), &(m->colunas));
+    alocamapa(m);
 
-	fclose(f);
+    for(int i = 0; i < m->linhas; i++) {
+        fscanf(f, "%s", m->matriz[i]);
+    }
+
+    fclose(f);
 }
 
+// Aloca memória para o mapa
 void alocamapa(MAPA* m) {
-	m->matriz = malloc(sizeof(char*) * m->linhas);
-
-	for(int i = 0; i < m->linhas; i++) {
-		m->matriz[i] = malloc(sizeof(char) * m->colunas + 1);
-	}
+    m->matriz = malloc(sizeof(char*) * m->linhas);
+    for(int i = 0; i < m->linhas; i++) {
+        m->matriz[i] = malloc(sizeof(char) * (m->colunas + 1));
+    }
 }
 
+// Copia um mapa para outro
 void copiamapa(MAPA* destino, MAPA* origem) {
-	destino->linhas = origem->linhas;
-	destino->colunas = origem->colunas;
-	alocamapa(destino);
-	for(int i = 0; i < origem->linhas; i++) {
-		strcpy(destino->matriz[i], origem->matriz[i]);
-	}
+    destino->linhas = origem->linhas;
+    destino->colunas = origem->colunas;
+    alocamapa(destino);
+    for(int i = 0; i < origem->linhas; i++) {
+        strcpy(destino->matriz[i], origem->matriz[i]);
+    }
 }
 
-
+// Libera a memória do mapa
 void liberamapa(MAPA* m) {
-	for(int i = 0; i < m->linhas; i++) {
-		free(m->matriz[i]);
-	}
-
-	free(m->matriz);
+    for(int i = 0; i < m->linhas; i++) {
+        free(m->matriz[i]);
+    }
+    free(m->matriz);
 }
 
-
+// Encontra a posição de um caractere no mapa
 int encontramapa(MAPA* m, POSICAO* p, char c) {
-
-	for(int i = 0; i < m->linhas; i++) {
-		for(int j = 0; j < m->colunas; j++) {
-			if(m->matriz[i][j] == c) {
-				p->x = i;
-				p->y = j;
-				return 1;
-			}
-		}
-	}
-
-	return 0;
+    for(int i = 0; i < m->linhas; i++) {
+        for(int j = 0; j < m->colunas; j++) {
+            if(m->matriz[i][j] == c) {
+                p->x = i;
+                p->y = j;
+                return 1;
+            }
+        }
+    }
+    return 0;
 }
 
+// Verifica se pode andar em determinada posição
 int podeandar(MAPA* m, char personagem, int x, int y) {
-	return 
-		ehvalida(m, x, y) && 
-		!ehparede(m, x, y) &&
-		!ehpersonagem(m, personagem, x, y);
+    return 
+        ehvalida(m, x, y) && 
+        !ehparede(m, x, y) &&
+        !ehpersonagem(m, personagem, x, y);
 }
 
+// Verifica se a posição é válida
 int ehvalida(MAPA* m, int x, int y) {
-	if(x >= m->linhas) 
-		return 0;
-	if(y >= m->colunas) 
-		return 0;
-
-	return 1;	
+    if(x < 0 || x >= m->linhas) 
+        return 0;
+    if(y < 0 || y >= m->colunas) 
+        return 0;
+    return 1;    
 }
 
+// Verifica se há um personagem na posição
 int ehpersonagem(MAPA* m, char personagem, int x, int y) {
-	return
-		m->matriz[x][y] == personagem;
+    return m->matriz[x][y] == personagem;
 }
 
+// Verifica se é parede
 int ehparede(MAPA* m, int x, int y) {
-	return 
-		m->matriz[x][y] == PAREDE_VERTICAL ||
-		m->matriz[x][y] == PAREDE_HORIZONTAL;
+    return 
+        m->matriz[x][y] == PAREDE_VERTICAL ||
+        m->matriz[x][y] == PAREDE_HORIZONTAL;
 }
 
-
-void andanomapa(MAPA* m, int xorigem, int yorigem, 
-	int xdestino, int ydestino) {
-
-	char personagem = m->matriz[xorigem][yorigem];
-	m->matriz[xdestino][ydestino] = personagem;
-	m->matriz[xorigem][yorigem] = VAZIO;
-
+// Move um personagem no mapa
+void andanomapa(MAPA* m, int xorigem, int yorigem, int xdestino, int ydestino) {
+    char personagem = m->matriz[xorigem][yorigem];
+    m->matriz[xdestino][ydestino] = personagem;
+    m->matriz[xorigem][yorigem] = VAZIO;
 }
